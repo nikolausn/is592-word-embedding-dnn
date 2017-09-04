277272db085fe32b374ea159e67770d29835135c
nikolausn <nikolaus.nova@gmail.com>
2017-09-04 17:45:23 -0500

    merge

--- embed_lstm.py
+++ embed_lstm.py
@@ -1,4 +1,4 @@
-# using tflearn make the graph creation simple
+#using tflearn make the graph creation simple
 import tensorflow as tf
 import tflearn
 import numpy as np
@@ -11,12 +11,12 @@ with open('traintest.pickle', 'rb') as f:
     trainX,testX,trainY,testY = pickle.load(f)
 
 with open('traintest-smote.pickle','rb') as f:
-	train_res,test_res = pickle.load(f)
+	train_smo_x,train_smo_y = pickle.load(f)
 
-test_res = to_categorical(test_res,nb_classes=len(np.unique(test_res)))
+train_smo_y = to_categorical(train_smo_y,nb_classes=len(np.unique(train_smo_y)))
 
 # Building the graph network
-batch_size=100
+batch_size=1000
 net = tflearn.input_data([None, 50])
 # reduce dimensionality using word embedding transform the name structure into 1000 dimensions
 net = tflearn.embedding(net,input_dim=62696,output_dim = 1000)
@@ -26,6 +26,18 @@ net = tflearn.fully_connected(net, 23, activation='softmax')
 net = tflearn.regression(net,optimizer='adam',learning_rate=0.001,loss='categorical_crossentropy')
 
 # Training
-model = tflearn.DNN(net,tensorboard_verbose=0,best_checkpoint_path='./best_check/best_',checkpoint_path='./check/check_')
+#tf.reset_default_graph()
+#with tf.Session() as session:
+#	new_saver = tf.train.import_meta_graph('check/check_-.meta',clear_devices=True)
+#	new_saver.restore(session, 'check/check_-360')
+model = tflearn.DNN(net,tensorboard_verbose=0,best_checkpoint_path='./best_check2/best_',checkpoint_path='./check2/check_')
+#with model.graph.as_default():
+#	with tf.Session() as session:
+#		new_saver = tf.train.import_meta_graph('check/check_-360.meta',clear_devices=True)
+#		new_saver.restore(session, 'check/check_-360')
 
-model.fit(trainX,trainY,validation_set=(testX,testY),show_metric=True,batch_size=batch_size,snapshot_epoch=True)
+	#model.fit(train_smo_x,train_smo_y,epoch=30,validation_set=(testX,testY),show_metric=True,batch_size=batch_size,snapshot_epoch=True)
+model.load('./check2/check_-792')
+model.fit(trainX,trainY,n_epoch=30,validation_set=(testX,testY),show_metric=True,batch_size=batch_size,snapshot_epoch=True)
+
+#model.save('./ethnicity.tflearn')
